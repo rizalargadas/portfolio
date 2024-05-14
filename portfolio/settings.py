34eb3c26aeffe69318df31235de52939c3e98ed2
Riza May Largadas <rmlargadas@gmail.com>
2024-05-15 05:53:26 +0800

    added whitenoise staticfiles_storage

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -116,6 +116,8 @@ DATABASES = {
     }
 }
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
