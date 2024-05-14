1a30b1de9de77a0258c24308dd61923950a6fbee
Riza May Largadas <rmlargadas@gmail.com>
2024-05-14 10:39:44 +0800

    railway deployment v4

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -151,7 +151,6 @@ MEDIA_ROOT = BASE_DIR / 'media'
 
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
-STATICFILES_DIRS = [BASE_DIR / 'media']
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
